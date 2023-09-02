c177bb78fedb416040c41bfb5d8f8204238037db
Dan Gohman <dev@sunfishcode.online>
2023-09-02 05:55:43 -0700

    Add an `origin-start-tiny example` demonstrating small code size.

---
+++ example-crates/origin-start-tiny/src/main.rs
@@ -0,0 +1,23 @@
+//! Going for minimal size!
+
+#![no_std]
+#![no_main]
+#![allow(internal_features)]
+#![feature(lang_items)]
+#![feature(core_intrinsics)]
+
+extern crate origin;
+extern crate compiler_builtins;
+
+#[panic_handler]
+fn panic(_panic: &core::panic::PanicInfo<'_>) -> ! {
+    core::intrinsics::abort()
+}
+
+#[lang = "eh_personality"]
+extern "C" fn eh_personality() {}
+
+#[no_mangle]
+extern "C" fn main(_argc: i32, _argv: *const *const u8) -> i32 {
+    42
+}

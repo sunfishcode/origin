6f8c04ed64a3373ae5636f6692926c6720f100b7
Federico Maria Morrone <contact@morrone.dev>
2023-09-05 18:37:55 +0200

    Create empty macro stub

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,14 +1,6 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use proc_macro::TokenStream;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[proc_macro_attribute]
+pub fn main(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    item
 }

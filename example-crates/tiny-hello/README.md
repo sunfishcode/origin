90a993f7ae80bfe6ede66bf03edfb585bc4da70a
Dan Gohman <dev@sunfishcode.online>
2024-10-08 14:42:15 -0700

    Add a version of the tiny example which prints hello world. (#137)

---
+++ example-crates/tiny-hello/README.md
@@ -0,0 +1,4 @@
+This crate is similar to the [tiny example], except that it prints
+"Hello, world!".
+
+[tiny example]: https://github.com/sunfishcode/origin/blob/main/example-crates/origin-start/README.md
---
+++ example-crates/tiny-hello/src/main.rs
@@ -0,0 +1,20 @@
+//! Going for minimal size!
+
+#![no_std]
+#![no_main]
+
+extern crate origin;
+
+#[no_mangle]
+unsafe fn origin_main(_argc: usize, _argv: *mut *mut u8, _envp: *mut *mut u8) -> i32 {
+    let message = b"Hello, world!\n";
+    let mut remaining = &message[..];
+    while !remaining.is_empty() {
+        match rustix::io::write(rustix::stdio::stdout(), message) {
+            Ok(n) => remaining = &remaining[n as usize..],
+            Err(rustix::io::Errno::INTR) => continue,
+            Err(_) => origin::program::abort(),
+        }
+    }
+    origin::program::exit_immediately(0);
+}
